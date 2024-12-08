88d242d2146adba1c3e2a3701d4e12a00de0fb82
kateiam <kryvonoskate125@gmail.com>
2024-12-08 18:29:55 +0200

    Add files via upload

---
+++ 10.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>null");
+
+    const int size = 10;
+    int ar1[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int ar2[size];
+
+    int* ptr1 = ar1;
+    int* ptr2 = ar2;
+
+    for (int i = 0; i < size; i++) {
+        *(ptr2 + i) = *(ptr1 + i);
+    }
+
+    cout << "Перший масив: ";
+    for (int i = 0; i < size; i++) {
+        cout << *(ptr1 + i) << " ";
+    }
+
+    cout << endl << "Другий масив, в який ми скопіювали елементи з першого: ";
+    for (int i = 0; i < size; i++) {
+        cout << *(ptr2 + i) << " ";
+    }
+}
---
+++ 10.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>null");
+
+    const int size = 10;
+    int ar[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    cout << "Початковий масив: ";
+    for (int i = 0; i < size; i++) {
+        cout << *(ar + i) << " ";
+    }
+
+    int* ptrPochatok = ar;
+    int* ptrKinezh = ar + size - 1;
+
+    while (ptrPochatok < ptrKinezh) {
+        int n = *ptrPochatok;
+        *ptrPochatok = *ptrKinezh;
+        *ptrKinezh = n;
+
+        ptrPochatok++;
+        ptrKinezh--;
+    }
+
+    cout << endl << "Масив в зворотньому порядку: ";
+    for (int i = 0; i < size; i++) {
+        cout << *(ar + i) << " ";
+    }
+}
